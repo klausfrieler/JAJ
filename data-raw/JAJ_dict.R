c953b65e710e39a3526a0f6717efe9c8b656f405
klaus <kgf@omniversum.de>
2021-12-14 20:35:18 +0100

    Added Italiano

--- data-raw/JAJ_dict.R
+++ data-raw/JAJ_dict.R
@@ -1,7 +1,7 @@
 #JAJ_dict_raw <- readRDS("data-raw/JAJ_dict.RDS")
 #JAJ_dict_raw <- read.table("data-raw/JAJ_dict_4.csv", sep = ",", stringsAsFactors = F, header = T, fileEncoding = "utf8")
 JAJ_dict_raw <- readxl::read_xlsx("data-raw/JAJ_dict.xlsx")
-JAJ_dict_raw <- JAJ_dict_raw[, c("key", "EN", "DE", "RU")]
+JAJ_dict_raw <- JAJ_dict_raw[, c("key", "EN", "DE", "RU", "IT")]
 JAJ_dict <- psychTestR::i18n_dict$new(JAJ_dict_raw, markdown = T)
 
 get_JAJ_dict <- function(){
